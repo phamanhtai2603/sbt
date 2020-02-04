4d9e49f54defd2dbca07b31ab0c22b7ebc80afdb
PAT <phamanhtai263@gmail.com>
2020-02-04 11:45:11 +0700

    after Like - Delay Job

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       end
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      redirect_to root_url
     end
   end
 
